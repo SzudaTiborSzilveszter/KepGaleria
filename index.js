010bdd191fc15c1ff06ee15e77cff03ed63529fd
SzudaTiborSzilveszter <szudatiborszilveszter@gmail.com>
2026-04-14 13:29:54 +0200

    04.14 Ora vege

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { KEPEKLISTA } from "./adat.js";
 import Kepek from "./Kepek.js";
 import KiemeltKep from "./KiemeltKep.js";
+import NagyKep from "./NagyKep.js";
 
 const szuloELEM = document.querySelector(".kartyatarto");
 const nagyKepELEM = document.querySelector(".kijelolt");
@@ -11,4 +12,5 @@ new Kepek(KEPEKLISTA,szuloELEM);
 window.addEventListener("kivalaszt", function(event) {
     nagyKepELEM.innerHTML=""
     new KiemeltKep(KEPEKLISTA[event.detail],nagyKepELEM)
+    new NagyKep(event.detail,KEPEKLISTA,nagyKepELEM)
 });
